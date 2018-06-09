9bd0524cf2302cc8d7ebc3dc40f5257abbdad190
Chiahao Lin <y80811@gmail.com>
2018-06-10 00:22:46 +0800

    chore: define base styling

---
+++ packages/task1-todolist/src/Root.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import './styles/base.scss'
+
+class Root extends Component {
+  render() {
+    return <h1>Root</h1>
+  }
+}
+
+export default Root
--- packages/task1-todolist/styleguide.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  require: ['normalize.css', path.join(__dirname, 'src/Root.scss')],
-  components: 'src/components/**/[A-Z]*.{js,jsx}',
-  skipComponentsWithoutExample: true
-}
